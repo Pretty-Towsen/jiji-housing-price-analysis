1b255d3697ac3479878ba2c07ece9c97f2cd35fc
oluwatosin <akannioluwatosin17@gmail.com>
2026-06-23 13:16:03 -0700

    initial commit

--- jiji_housing_analysis_dashboard.py
+++ jiji_housing_analysis_dashboard.py
@@ -17,7 +17,7 @@ st.set_page_config(
 @st.cache_data
 def load_data():
     try:
-        df = pd.read_csv("data/nigerian-housing-cleaned-dataset.csv")
+        df = pd.read_csv("data/jiji_housing_apartment_cleaned.csv")
         return df
     except FileNotFoundError as e:
         st.warning(f"An error occured: {e}")
@@ -27,25 +27,25 @@ def create_sidebar_filters(df):
 
     Region = st.sidebar.multiselect(
         "Select State(s):", 
-        options=df['Region'].unique(), 
-        default=df['Region'].unique()
+        options=df['Region_Parent_Name'].unique(), 
+        default=df['Region_Parent_Name'].unique()
     )
 
-    price_title = st.sidebar.multiselect(
-        "Selected price_title(s)",
-        options=df['price_title'].unique(),
-        default=df['price_title'].unique()
+    Furnishing = st.sidebar.multiselect(
+        "Selected Furnishing(s)",
+        options=df['Furnishing'].unique(),
+        default=df['Furnishing'].unique()
     )
-    boosting = st.sidebar.multiselect(
+    Boosting = st.sidebar.multiselect(
         'Selected Boost(s)',
-        options=df['is_boost'].unique(),
-        default=df['is_boost'].unique()
+        options=df['Is_Boost'].unique(),
+        default=df['Is_Boost'].unique()
     ) 
 
-    return Region, price_title, boosting
+    return Region, Furnishing, Boosting
 
-def  filter_data(df, Region, price_title, boosting):
-    filtered_df = df[df['Region'].isin(Region) & df['price_title'].isin(price_title) & df['is_boost'].isin(boosting)]
+def  filter_data(df, Region_Parent_Name, Furnishing, Boosting):
+    filtered_df = df[df['Region_Parent_Name'].isin(Region_Parent_Name) & df['Furnishing'].isin(Furnishing) & df['Is_Boost'].isin(Boosting)]
     return filtered_df
 
 def display_metrics(filtered_df):
@@ -55,16 +55,18 @@ def display_metrics(filtered_df):
         st.metric("🏠Total House Listings", len(filtered_df))
 
     with col2:
-        avg_price = filtered_df['price_title'].mean() if len(filtered_df) > 0 else 0
-        st.metric("⛪Average Price", f"₦{avg_price:,.2f}")
+        avg_price = (filtered_df['Price'].mean() /1_000_000) if len(filtered_df) > 0 else 0
+        st.metric("⛪Average Price", f"₦{avg_price:,.2f}M")
 
     with col3:
-        common_region = filtered_df['Region'].mode()[0] if not filtered_df.empty else "N/A"
+        common_region = filtered_df['Region_Parent_Name'].mode()[0] if not filtered_df.empty else "N/A"
         st.metric("🏣Most Common Region", value=common_region)
 
     with col4:
-        house_pct = (filtered_df['furnishing'] == 'Furnished').sum() / len(filtered_df) * 100 if len(filtered_df) > 0 else 0
-        st.metric("✔ house percentage", f"{house_pct:.1f}%")
+        house_pct = (filtered_df['Furnishing'] == 'Furnished').sum() / len(filtered_df) * 100 if len(filtered_df) > 0 else 0
+        st.metric("✔ house percentage", f"{house_pct:.2f}%")
+
+
 
     
 def display_chart(filtered_df):
@@ -74,11 +76,11 @@ def display_chart(filtered_df):
     
     col1, col2 = st.columns(2)
     with col1:
-       state_counts = filtered_df['Region'].value_counts().reset_index()
-       state_counts.columns = ['Region', 'Listings']
+       state_counts = filtered_df['Region_Parent_Name'].value_counts().reset_index()
+       state_counts.columns = ['Region_Parent_Name', 'Listings']
        fig1 = px.bar(
        state_counts,
-       x='Region',
+       x='Region_Parent_Name',
        y='Listings',
        title='Number of Listings per Region'
        )
@@ -86,11 +88,11 @@ def display_chart(filtered_df):
        st.plotly_chart(fig1, width='stretch')
 
     with col2:
-        avg_price_state = filtered_df.groupby('Region')['price_title'].mean().sort_values(ascending=False).reset_index()
+        avg_price_state = filtered_df.groupby('Region_Parent_Name')["Price"].mean().sort_values(ascending=False).reset_index()
         fig2 = px.bar(
         avg_price_state,
-        x='Region',
-        y='price_title',
+        x='Region_Parent_Name',
+        y='Price',
         title='Average Property Price per Region'
         )
         st.plotly_chart(fig2, width='stretch')
@@ -98,7 +100,7 @@ def display_chart(filtered_df):
     col3, col4 = st.columns(2)
     with col3:
         fig3 = px.histogram(filtered_df,
-        x='price_title',
+        x='Price',
         nbins=50,
         title='Distribution of Property Prices'
         )
@@ -106,12 +108,12 @@ def display_chart(filtered_df):
 
     with col4:
         fig4 = px.box(filtered_df,
-        x='bedrooms',
-        y='price_title',
+        x='Bedrooms',
+        y='Price',
         title='Property Price by Number of Bedrooms',
         labels={
-        'bedrooms':'Bedrooms',
-        'price_title':'price_title (₦)'
+        'Bedrooms':'Bedrooms',
+        'Price':'Price (₦)'
         })
         st.plotly_chart(fig4, width='stretch')
         
@@ -119,14 +121,14 @@ def display_chart(filtered_df):
     with col5:
         fig5 = px.scatter(
         filtered_df,
-        x='bedrooms',
-        y='price_title',
+        x='Bedrooms',
+        y='Price',
         title='Property Size vs Price'
         )
         st.plotly_chart(fig5, width='stretch')
 
     with col6:
-        furnishing_counts = filtered_df['furnishing'].value_counts().reset_index()
+        furnishing_counts = filtered_df['Furnishing'].value_counts().reset_index()
         furnishing_counts.columns = ['Furnishing', 'Count']
         fig6 = px.pie(
         furnishing_counts,
@@ -138,7 +140,7 @@ def display_chart(filtered_df):
 
     col7 = st.columns(1)[0]
     with col7:
-        corr_df = filtered_df[['price_title', 'property_size', 'bedrooms', 'bathrooms']].corr()
+        corr_df = filtered_df[['Price', 'Property_Size', 'Bedrooms', 'Bathrooms']].corr()
         fig7 = ff.create_annotated_heatmap(
         z=corr_df.values,
         x=list(corr_df.columns),
@@ -153,7 +155,7 @@ def display_table_data(filtered_df):
    if len(filtered_df) > 0:
       st.dataframe(filtered_df, width='stretch', height=300)
    else:
-      st.warning("No employee data to display")
+      st.warning("No housing data to display")
 
 
 
@@ -168,10 +170,10 @@ def main():
     #load dataset
     df = load_data()
     #sidebar
-    Region, price_title, boosting = create_sidebar_filters(df)
+    Region, Furnishing, Boosting = create_sidebar_filters(df)
     #filtered_data
-    filtered_df = filter_data(df, Region, price_title, boosting)
-    st.title("Jiji Analysis Dashboard")
+    filtered_df = filter_data(df, Region, Furnishing, Boosting)
+    st.title("Jiji Housing Price Dashboard")
     st.markdown("---")
     #display metrics
     display_metrics(filtered_df)
@@ -180,4 +182,18 @@ def main():
     #display_table_data
     st.markdown("---")
     display_table_data(filtered_df)
+    st.subheader("Insights And Recommendations")
+
+    st.markdown("""
+            Large furnished homes with multiple bedrooms generate the highest market value on jiji.
+            Lagos provides the greatest revenue potential due to its concentration of premium listings.
+            Abuja also performs strongly in high-value property sales.
+            Less urbanized states show lower demand and lower pricing power.
+            Sellers in low-performing regions should invest in better listing descriptions, quality images, and competitive pricing.
+            Boosting listings can improve visibility and attract more potential buyers.
+            Highlighting furnishing status, property size, and premium amenities can justify higher prices and increase sales performance.
+
+            """)
+
+
 main()
